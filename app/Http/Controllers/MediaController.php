a732b58f2e1b4c62cc424adf8b94afc2023799ea
Lukas Heller <dev@lukas-heller.de>
2023-02-10 15:20:36 +0100

    update media converions controller

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
-use Intervention\Image\ImageCache;
 
 class MediaController extends Controller
 {
@@ -18,24 +17,77 @@ class MediaController extends Controller
      */
     public function conversion(Request $request, $id, $file)
     {
-        $path = storage_path("app/public/{$id}/{$file}");
+        $originalPath = storage_path("app/public/{$id}/{$file}");
 
-        if (! file_exists($path)) {
+        // abort if original file does not exist
+        if (! file_exists($originalPath)) {
             abort(404);
         }
 
-        $img = Image::cache(function (ImageCache $image) use ($path, $request) {
-            $img = $image->make($path);
+        $path = $originalPath;
 
-            $h = $request->h;
-            $w = $request->w;
-            if ($h || $w) {
-                $img->resize($w, $h, function ($constraint) {
-                    $constraint->aspectRatio();
-                });
-            }
-        }, 60 * 24 * 365, true);
+        $h = $this->roundImageSize($request->h);
+        $w = $this->roundImageSize($request->w);
 
-        return $img->response();
+        if ($h) {
+            $path = ($this->addStringToFilename($path, '_h'.$h));
+        }
+        if ($w) {
+            $path = ($this->addStringToFilename($path, '_w'.$w));
+        }
+
+        // if the requested image size does exist, return it
+        if (file_exists($path)) {
+            return response()->file($path);
+        }
+
+        $img = Image::make($originalPath);
+
+        // if the image width is smaller than the requested width, don't upscale,
+        // use the original image.
+        if (($w != null && $w > $img->width()) || ($h != null && $h > $img->height())) {
+            return response()->file($originalPath);
+        }
+
+        $img->resize($w, $h, function ($constraint) {
+            $constraint->aspectRatio();
+        });
+
+        $img->save($path, 60);
+
+        return response()->file($path);
+    }
+
+    public function addStringToFilename($filename, $string)
+    {
+        $ext = pathinfo($filename, PATHINFO_EXTENSION);
+        $filename = str_replace('.'.$ext, '', $filename).$string.'.'.$ext;
+
+        return $filename;
+    }
+
+    public function roundImageSize(int|null $size = null)
+    {
+        if ($size == null) {
+            return;
+        }
+        if ($size < 10) {
+            return 10;
+        }
+        if ($size < 100) {
+            return (int) ceil($size / 10) * 10;
+        }
+        if ($size < 1600) {
+            $f = $size % 50;
+
+            return $size - $f + ($f > 0 ? 50 : 0);
+        }
+        if ($size < 2800) {
+            $f = $size % 100;
+
+            return $size - $f + ($f > 0 ? 100 : 0);
+        }
+
+        return 2800;
     }
 }
