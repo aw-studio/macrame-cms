03b2f45e1b4e105afa50b2f29634ec056c0cf3e1
Lukas Heller <dev@lukas-heller.de>
2023-02-11 10:52:38 +0100

    add features to app config

--- admin/resources/app/config/app.ts
+++ admin/resources/app/config/app.ts
@@ -1,3 +1,10 @@
 export default {
     url: import.meta.env.VITE_APP_URL,
+    // enable or disable certain features
+    features: {
+        posts: true, 
+        events: false,
+        people: false,
+        blocks: false,
+    },
 };
